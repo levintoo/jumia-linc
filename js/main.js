81cade6f05ba90b0ce79307c3463c4711c5e7a52
levintoo <tooklevin@gmail.com>
2021-06-17 11:11:39 +0300

    js object

--- js/main.js
+++ js/main.js
@@ -6,9 +6,59 @@
 //  src.appendChild(img);
 
 
+
 const obj1 ={
-    product1name ="Sansung jomii",
-    product1price= 3000,
-    product1image= "images/p1/1.jpg"
+    product1name :"Sansung jomii" ,
+    product1price: 3000,
+    product1image: "images/p1/1.jpg",
+    product1oldprice: 4500,
+    
 }
-document.getElementById("product1Image")
+document.getElementById("product1Image").src=obj1.product1image;
+  document.getElementById("product1Name").innerHTML=obj1.product1name;
+  document.getElementById("product1Price").innerHTML=obj1.product1price;
+  document.getElementById("product1oldprice").innerHTML=obj1.product1oldprice;
+
+  
+const obj2 ={
+    product2name :"Sansung UHD TV" ,
+    product2price: 12000,
+    product2image: "images/tv1/1.jpg",
+    product2oldprice: 1500,
+}
+document.getElementById("product2Image").src=obj2.product2image;
+  document.getElementById("product2Name").innerHTML=obj2.product2name;
+  document.getElementById("product2Price").innerHTML=obj2.product2price;
+  document.getElementById("product2oldprice").innerHTML=obj2.product2oldprice;
+
+
+  const obj3 ={
+    product3name :"Fashonable Sansung wear" ,
+    product3price: 2000,
+    product3image: "images/cloth1/1.jpg",
+    product3oldprice: 8500,
+}
+document.getElementById("product3Image").src=obj3.product3image;
+  document.getElementById("product3Name").innerHTML=obj3.product3name;
+  document.getElementById("product3Price").innerHTML=obj3.product3price;
+  document.getElementById("product3oldprice").innerHTML=obj3.product3oldprice;
+
+
+
+  
+  const obj4 ={
+    product4name :"Fashonable Sansung wear" ,
+    product4price: 249000 ,
+    product4image: "images/mac1/1.jpg",
+    product4oldprice: 249001,
+}
+document.getElementById("product4Image").src=obj4.product4image;
+  document.getElementById("product4Name").innerHTML=obj4.product4name;
+  document.getElementById("product4Price").innerHTML=obj4.product4price;
+  document.getElementById("product4oldprice").innerHTML=obj4.product4oldprice;
+
+
+
+
+
+
