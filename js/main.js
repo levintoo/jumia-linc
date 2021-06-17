dd2965cc48a070eda2eaff1748c5539fc967735a
levintoo <tooklevin@gmail.com>
2021-06-17 10:20:46 +0300

    ids at work

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// var img = document.createElement("img");
+
+//  img.src = "images/main.png"
+//  var src = document.getElementById("col2");
+
+//  src.appendChild(img);
+
+
+const obj1 ={
+    product1name ="Sansung jomii",
+    product1price= 3000,
+    product1image= "images/p1/1.jpg"
+}
+document.getElementById("product1Image")
--- main.js
+++
@@ -1,7 +0,0 @@
-// var img = document.createElement("img");
-
-//  img.src = "images/main.png"
-//  var src = document.getElementById("col2");
-
-//  src.appendChild(img);
-
